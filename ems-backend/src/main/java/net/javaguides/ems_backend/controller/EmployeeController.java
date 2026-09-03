4a16301b42e4ad8b13fbd4a2b8170d3ac4a858c5
haroldo-barros <debarrosharoldo@gmail.com>
2026-09-03 16:53:23 -0400

    build Get Employee rest api

--- ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -23,6 +20,14 @@ public class EmployeeController {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
 
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // build Get Employe Rest API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+
+        return ResponseEntity.ok(employeeDto);
     }
 
 }
--- ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.ems_backend.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourceNotFoundException;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
 import net.javaguides.ems_backend.repository.EmployeeRepository;
 import net.javaguides.ems_backend.service.EmployeeService;
@@ -21,5 +22,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
 
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee Does Not Exist with Given ID : " + employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
